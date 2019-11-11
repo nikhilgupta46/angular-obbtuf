145a71b2ee4757e2b8d3722ae076263710bc9ad2
nikhilgupta46 <bluehell46@gmail.com>
2019-11-11 16:34:00 +0530

    1 file modified, 7 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,13 @@ import {HomeComponent} from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { AuthService } from './auth.service';
-import {AppRoutingModule } from './approuting.module';
+import {AppRoutingModule } from './app-routing.module';
 import { MenuComponent } from './menu/menu.component';
 import { MyblogsComponent } from './myblogs/myblogs.component';
 import { CreateComponent } from './create/create.component';
 import {NgxEditorModule} from 'ngx-editor';
 import {HttpClientModule} from '@angular/common/http';
-import { PostComponent } from './post/post.component';
-import { ViewComponent } from './view/view.component';
-import { ProfileComponent } from './profile/profile.component'; 
+
 let config = {
     apiKey: "AIzaSyATB_vE19LCfsmIfhf1feh1SsaZ8jtKwQE",
     authDomain: "blog8-48335.firebaseapp.com",
